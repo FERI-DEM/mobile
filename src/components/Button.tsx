3ab629a1f73f1208cd6804becc4429c45f8695da
Matevz <semprimoznik.matevz@gmail.com>
2023-03-12 18:48:21 +0100

    feat: add button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,25 @@
+import React, {FC, ReactNode} from 'react';
+import {Pressable, View, Text, TouchableOpacity} from "react-native";
+import {twMerge} from "tailwind-merge";
+
+interface ButtonProps {
+    children?: ReactNode;
+    text: string;
+    onPress: () => void;
+    classname?: string;
+    disabled?: boolean;
+    classNameText?: string;
+}
+
+const Button:FC<ButtonProps> = ({ children, text, onPress, classname, classNameText }) => {
+    return (
+        <View className={twMerge('bg-tint py-2.5 px-6 rounded-md', classname)}>
+            <TouchableOpacity onPress={onPress}>
+                <Text className={twMerge('text-white font-bold', classNameText)}>{text}</Text>
+                {children}
+            </TouchableOpacity>
+        </View>
+    );
+};
+
+export default Button;
