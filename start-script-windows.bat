6f210d2380b5f90340cecb42cb9e0d058d540a7c
Matevz <semprimoznik.matevz@gmail.com>
2023-04-12 09:43:40 +0200

    fix: update start-script for windows

--- start-script-windows.bat
+++ start-script-windows.bat
@@ -3,15 +3,13 @@ setlocal enabledelayedexpansion
 
 set "ip_address="
 for /f "tokens=2 delims=:" %%i in ('ipconfig ^| findstr /c:"IPv4 Address"') do (
-set "ip_address=%%i"
-set "ip_address=!ip_address: =!"
-goto :continue
+  set "ip_address=%%i"
+  set "ip_address=!ip_address: =!"
 )
 
-:continue
 if not defined ip_address (
-echo IP address was not found.
-exit /b 1
+  echo IP address was not found.
+  exit /b 1
 )
 
 set "file=.env.development"
