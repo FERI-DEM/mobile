4b9d99d2305126189452170c6f62d2203898779a
Matevz <semprimoznik.matevz@gmail.com>
2023-04-15 11:52:58 +0200

    fix: community screen header

--- src/navigation/HeaderBar.tsx
+++ src/navigation/HeaderBar.tsx
@@ -1,22 +1,19 @@
 import {Text, TouchableOpacity, View} from "react-native";
 import Svg, {Path} from "react-native-svg";
 import {useSideMenuStore} from "../store/side-menu-store";
-import {ReactElement} from "react";
 
 interface HeaderBarProps {
     title: string
-    helper?: ReactElement
 }
-const HeaderBar = ({title, helper}: HeaderBarProps) => {
+const HeaderBar = ({title}: HeaderBarProps) => {
     const {toggleOpened} = useSideMenuStore()
 
     return (
         <View className='flex justify-between items-center flex-row dark:bg-dark-main p-4'>
             <View className='flex flex-row items-center'>
                 <Text className='text-2xl text-white font-bold'>{title}</Text>
-                {helper && helper}
             </View>
-            <TouchableOpacity activeOpacity={0.6} onPress={toggleOpened}>
+            <TouchableOpacity activeOpacity={0.6} onPress={toggleOpened} className='pt-1.5'>
                 <View className='w-7 h-7'>
                     <Svg width="100%" height='100%' viewBox="0 0 24 24" fill="none">
                         <Path d="M4 6H20M4 10H20M4 14H20M4 18H20" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
--- src/navigation/UserStack.tsx
+++ src/navigation/UserStack.tsx
@@ -10,21 +10,18 @@ import MyCommunityScreen from "../screens/MyCommunityScreen";
 import SideMenu from "./SideMenu";
 import * as React from "react";
 import SettingsScreen from "../screens/SettingsScreen";
-import {useCommunityStore} from "../store/community-store";
-import CommunityHeaderDropdown from "../components/CommunityHeaderDropdown";
+import HeaderBarCommunities from "./HeaderBarCommunities";
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const UserStack = () => {
-    const selectedCommunity = useCommunityStore(state => state.selectedCommunity);
-
     return (
         <SafeAreaView className='flex-1'>
             <Stack.Navigator initialRouteName={userStackInitialRoute} screenOptions={{headerShown: true, animation: 'none', header: (props) => <HeaderBar title={props.options.title || props.route.name}/>}}>
                 <Stack.Screen name={Routes.DASHBOARD} component={DashboardScreen}/>
                 <Stack.Screen name={Routes.NOTIFICATIONS} component={NotificationScreen}  />
                 <Stack.Screen name={Routes.CALIBRATION} component={CalibrationScreen} />
-                <Stack.Screen name={Routes.ORGANIZATION} component={MyCommunityScreen} options={{ headerShown: true, header: () => <HeaderBar title={selectedCommunity.name} helper={<CommunityHeaderDropdown/>}/>}} />
+                <Stack.Screen name={Routes.ORGANIZATION} component={MyCommunityScreen} options={{header: () => <HeaderBarCommunities/>}} />
                 <Stack.Screen name={Routes.SETTINGS} component={SettingsScreen} />
             </Stack.Navigator>
             <SideMenu/>
