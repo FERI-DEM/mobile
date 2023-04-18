d284dd0c32ff9469c23b50561a6b4d197c861fae
gregorsulcer <gregor.sulcer@student.um.si>
2023-04-18 12:14:10 +0200

    fix: iOS main view

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,6 +1,6 @@
 import {NavigationContainer, DefaultTheme, DarkTheme} from '@react-navigation/native';
 import * as React from 'react';
-import {ColorSchemeName, Text} from 'react-native';
+import {ColorSchemeName, Text, View} from 'react-native';
 import LinkingConfiguration from './LinkingConfiguration';
 import {navigationRef} from "./navigate";
 import {useAuthentication} from "../hooks/useAuthentication";
@@ -9,12 +9,14 @@ import NoUserStack from "./NoUserStack";
 
 export default function Navigation({colorScheme}: { colorScheme: ColorSchemeName }) {
     return (
-        <NavigationContainer
-            ref={navigationRef}
-            linking={LinkingConfiguration}
-            theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-            <RootNavigator/>
-        </NavigationContainer>
+        <View className='flex-1 bg-dark-main'>
+            <NavigationContainer
+                ref={navigationRef}
+                linking={LinkingConfiguration}
+                theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+                <RootNavigator/>
+            </NavigationContainer>
+        </View>
     );
 }
 
