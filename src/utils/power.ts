b283e0ac13afd777c8ab5ec8f68d1af855f8faf2
jernej10 <jernej.ofic@gmail.com>
2023-05-16 17:04:41 +0200

    feat: display alert on energy drop

--- src/utils/power.ts
+++ src/utils/power.ts
@@ -4,3 +4,11 @@ export const roundToTwoDecimalPlaces = (num: number) => {
     else
         return Number(num.toFixed(2));
 }
+
+export const calculatePowerPercentageDifference = (firstNumber: number, secondNumber: number) => {
+    const difference = firstNumber - secondNumber;
+    const percentageDifference = (difference / firstNumber) * 100;
+    if (percentageDifference >= 50)
+        return Math.trunc(percentageDifference);
+    return null;
+};
