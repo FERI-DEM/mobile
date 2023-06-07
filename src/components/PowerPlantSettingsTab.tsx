bbae114944367701586ba3d2d3a32e58b1e973aa
jernej10 <jernej.ofic@gmail.com>
2023-06-07 14:49:01 +0200

    fix: edit & delete powerPlant

--- src/components/PowerPlantSettingsTab.tsx
+++ src/components/PowerPlantSettingsTab.tsx
@@ -1,82 +1,122 @@
-import {ScrollView, View} from "react-native";
-import Button from "./Button";
-import React from "react";
-import {usePowerPlantStore} from "../store/power-plant-store";
-import {UpdatePowerPlantDataType} from "../types/powerPlant.types";
-import {zodResolver} from "@hookform/resolvers/zod/dist/zod";
-import {UpdatePowerPlantDataSchema} from "../schemas/powerPlant.schema";
-import {FormProvider, SubmitHandler} from "react-hook-form";
-import {ControlledInput} from "./ControlledInput";
-import usePowerPlant from "../hooks/usePowerPlant";
-import usePowerPlantDeleteMutation from "../hooks/usePowerPlantDeleteMutation";
-import {useQueryClient} from "@tanstack/react-query";
-import {QueryKey} from "../types/keys.types";
-import useForm from "../hooks/useForm";
-import {PowerPlantsTab, useDashboardTabsStore} from "../store/dashboard-tabs-store";
-import {useToastStore} from "../store/toast-store";
-import {ToastTypes} from "../types/toast.types";
-import usePowerPlantUpdateMutation from "../hooks/usePowerPlantUpdateMutation";
+import { ScrollView, View } from 'react-native';
+import Button from './Button';
+import React from 'react';
+import { usePowerPlantStore } from '../store/power-plant-store';
+import { UpdatePowerPlantDataType } from '../types/powerPlant.types';
+import { zodResolver } from '@hookform/resolvers/zod/dist/zod';
+import { UpdatePowerPlantDataSchema } from '../schemas/powerPlant.schema';
+import { FormProvider, SubmitHandler } from 'react-hook-form';
+import { ControlledInput } from './ControlledInput';
+import usePowerPlant from '../hooks/usePowerPlant';
+import usePowerPlantDeleteMutation from '../hooks/usePowerPlantDeleteMutation';
+import { useQueryClient } from '@tanstack/react-query';
+import { QueryKey } from '../types/keys.types';
+import useForm from '../hooks/useForm';
+import {
+  PowerPlantsTab,
+  useDashboardTabsStore,
+} from '../store/dashboard-tabs-store';
+import { useToastStore } from '../store/toast-store';
+import { ToastTypes } from '../types/toast.types';
+import usePowerPlantUpdateMutation from '../hooks/usePowerPlantUpdateMutation';
+import usePowerPlants from '../hooks/usePowerPlants';
+import { navigate } from '../navigation/navigate';
+import { Routes } from '../navigation/routes';
 
 const PowerPlantSettingsTab = () => {
+  const queryClient = useQueryClient();
+  const setActiveTab = useDashboardTabsStore((state) => state.setActiveTab);
+  const { setSelectedPowerPlant, selectedPowerPlant } = usePowerPlantStore();
 
-    const queryClient = useQueryClient()
-    const setActiveTab = useDashboardTabsStore(state => state.setActiveTab)
-    const selectedPowerPlant = usePowerPlantStore(state => state.selectedPowerPlant);
-    const {data: powerPlantData} = usePowerPlant(selectedPowerPlant?.id || '', {enabled: !!selectedPowerPlant})
-    const form = useForm<UpdatePowerPlantDataType>({
-        resolver: zodResolver(UpdatePowerPlantDataSchema),
-        defaultValues: {name: powerPlantData?.powerPlants[0].displayName || ""}
-    })
-    const { showToast } = useToastStore();
+  const { data: powerPlantData } = usePowerPlant(selectedPowerPlant?.id || '', {
+    enabled: !!selectedPowerPlant,
+  });
 
+  const { data: powerPlants, refetch: refetchPowerPlants } = usePowerPlants();
+  console.log(powerPlants);
 
-    const {mutate: deletePowerPlant} = usePowerPlantDeleteMutation(selectedPowerPlant?.id || '', {
-        onSuccess: () => {
-            showToast('Elektrarna uspešno izbrisana!', ToastTypes.SUCCESS)
-            queryClient.invalidateQueries({ queryKey: [QueryKey.POWER_PLANTS] }, {})
-            setActiveTab(PowerPlantsTab.DASHBOARD)
-        },
-        onError: (err) => {
-            showToast('Napaka pri brisanju elektrarne!', ToastTypes.ERROR)
+  const form = useForm<UpdatePowerPlantDataType>({
+    resolver: zodResolver(UpdatePowerPlantDataSchema),
+    defaultValues: { name: powerPlantData?.powerPlants[0].displayName || '' },
+  });
+
+  const { showToast } = useToastStore();
+
+  const { mutate: deletePowerPlant } = usePowerPlantDeleteMutation(
+    selectedPowerPlant?.id || '',
+    {
+      onSuccess: async () => {
+        showToast('Elektrarna uspešno izbrisana!', ToastTypes.SUCCESS);
+        const powerPlants = await refetchPowerPlants();
+        if (powerPlants?.data?.length === 0) {
+          navigate(Routes.ADD_POWER_PLANT);
+        } else if (powerPlants?.data) {
+          setSelectedPowerPlant({
+            id: powerPlants?.data[0]._id,
+            name: powerPlants?.data[0].displayName,
+          });
         }
-    })
+        setActiveTab(PowerPlantsTab.DASHBOARD);
+      },
+      onError: (err) => {
+        showToast('Napaka pri brisanju elektrarne!', ToastTypes.ERROR);
+      },
+    }
+  );
 
-    const {mutate: updatePowerPlant} = usePowerPlantUpdateMutation(selectedPowerPlant?.id || '', {
-        onSuccess: () => {
-            showToast('Elektrarna uspešno posodobljena!', ToastTypes.SUCCESS)
-            queryClient.invalidateQueries({ queryKey: [QueryKey.POWER_PLANTS] }, {})
-        },
-        onError: (err) => {
-            showToast('Napaka pri posodabljanju elektrarne!', ToastTypes.ERROR)
-        }
-    })
+  const { mutate: updatePowerPlant } = usePowerPlantUpdateMutation(
+    selectedPowerPlant?.id || '',
+    {
+      onSuccess: () => {
+        showToast('Elektrarna uspešno posodobljena!', ToastTypes.SUCCESS);
+        queryClient.invalidateQueries(
+          { queryKey: [QueryKey.POWER_PLANTS] },
+          {}
+        );
+        setSelectedPowerPlant({
+          id: selectedPowerPlant?.id || '',
+          name: form.getValues()?.name || '',
+        });
+      },
+      onError: (err) => {
+        showToast('Napaka pri posodabljanju elektrarne!', ToastTypes.ERROR);
+      },
+    }
+  );
 
-    const onSubmit: SubmitHandler<UpdatePowerPlantDataType> = (data) => {
-        // TODO add max power & size
-        //updatePowerPlant({ displayName: data.name, latitude: powerPlantData!.powerPlants[0].latitude, longitude: powerPlantData!.powerPlants[0].longitude})
-    };
+  const onSubmit: SubmitHandler<UpdatePowerPlantDataType> = (data) => {
+    updatePowerPlant({
+      displayName: data.name,
+      latitude: powerPlantData!.powerPlants[0].latitude,
+      longitude: powerPlantData!.powerPlants[0].longitude,
+    });
+  };
 
-    return (
-        <View className='dark:bg-dark-main flex-1 px-3'>
-            <ScrollView className='mt-5 w-full' keyboardShouldPersistTaps='always'>
-                <View className='px-2'>
-                    <FormProvider {...form}>
-                        <ControlledInput
-                            name="name"
-                            label="Ime elektrarne"
-                            placeholder="Ime"
-                            defaultValue={powerPlantData?.powerPlants[0].displayName}
-                        />
-                        <Button
-                            text='Posodobi'
-                            classname='mt-4'
-                            onPress={form.handleSubmit(onSubmit)}
-                        />
-                    </FormProvider>
-                </View>
-            </ScrollView>
-            <Button text='Izbriši elektrarno' onPress={deletePowerPlant} classname='bg-danger m-auto my-4'/>
+  return (
+    <View className="dark:bg-dark-main flex-1 px-3">
+      <ScrollView className="mt-5 w-full" keyboardShouldPersistTaps="always">
+        <View className="px-2">
+          <FormProvider {...form}>
+            <ControlledInput
+              name="name"
+              label="Ime elektrarne"
+              placeholder="Ime"
+              defaultValue={powerPlantData?.powerPlants[0].displayName}
+            />
+            <Button
+              text="Posodobi"
+              classname="mt-4"
+              onPress={form.handleSubmit(onSubmit)}
+            />
+          </FormProvider>
         </View>
-    )
-}
-export default PowerPlantSettingsTab
+      </ScrollView>
+      <Button
+        text="Izbriši elektrarno"
+        onPress={deletePowerPlant}
+        classname="bg-danger m-auto my-4"
+      />
+    </View>
+  );
+};
+export default PowerPlantSettingsTab;
--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -1,34 +1,47 @@
-import * as z from "zod";
+import * as z from 'zod';
 
 export const BaseRegisterSchema = z.object({
-    email: z.string().min(1, { message: "Elektronski naslov je obvezen" }).email({
-        message: "Vpisan elektronski naslov ni veljaven"
-    }),
-    password: z.string().min(6, { message: "Geslo mora vsebovati vsaj 6 znakov" }),
-    confirmPassword: z.string().min(6, { message: "Geslo mora vsebovati vsaj 6 znakov" }),
+  email: z.string().min(1, { message: 'Elektronski naslov je obvezen' }).email({
+    message: 'Vpisan elektronski naslov ni veljaven',
+  }),
+  password: z
+    .string()
+    .min(6, { message: 'Geslo mora vsebovati vsaj 6 znakov' }),
+  confirmPassword: z
+    .string()
+    .min(6, { message: 'Geslo mora vsebovati vsaj 6 znakov' }),
 });
 
 export const AddPowerPlantSchema = z.object({
-    powerPlantName: z.string().min(3, { message: "Ime elektrarne mora biti dolgo vsaj 3 znake" }).max(20, { message: "Ime elektrarne ne sme biti daljše od 20 znakov" }),
-    location: z.string().min(1, { message: "Location must be at least 1 character long" }),
-    maxPower: z.number().min(1, { message: "Maksimalna moč mora biti večja od 0" }),
-    size: z.number().min(1, { message: "Velikost elektrarne mora biti večja od 0" }),
+  powerPlantName: z
+    .string()
+    .min(3, { message: 'Ime elektrarne mora biti dolgo vsaj 3 znake' })
+    .max(20, { message: 'Ime elektrarne ne sme biti daljše od 20 znakov' }),
+  location: z
+    .string()
+    .min(1, { message: 'Lokacija mora biti dolgo vsaj 1 znak' }),
 });
 
 export const SignInCredentialsSchema = z.object({
-    email: z.string().min(1, { message: "Elektronski naslov je obvezen" }).email({
-        message: "Vpisan elektronski naslov ni veljaven"
-    }),
-    password: z.string().min(6, { message: "Geslo mora vsebovati vsaj 6 znakov" }),
-})
+  email: z.string().min(1, { message: 'Elektronski naslov je obvezen' }).email({
+    message: 'Vpisan elektronski naslov ni veljaven',
+  }),
+  password: z
+    .string()
+    .min(6, { message: 'Geslo mora vsebovati vsaj 6 znakov' }),
+});
 
 export const ChangePasswordSchema = z.object({
-    password: z.string().min(6, { message: "Geslo mora vsebovati vsaj 6 znakov" }),
-    confirmPassword: z.string().min(6, { message: "Geslo mora vsebovati vsaj 6 znakov" }),
+  password: z
+    .string()
+    .min(6, { message: 'Geslo mora vsebovati vsaj 6 znakov' }),
+  confirmPassword: z
+    .string()
+    .min(6, { message: 'Geslo mora vsebovati vsaj 6 znakov' }),
 });
 
 export const ForgotPasswordSchema = z.object({
-    email: z.string().min(1, { message: "Elektronski naslov je obvezen" }).email({
-        message: "Vpisan elektronski naslov ni veljaven"
-    })
+  email: z.string().min(1, { message: 'Elektronski naslov je obvezen' }).email({
+    message: 'Vpisan elektronski naslov ni veljaven',
+  }),
 });
