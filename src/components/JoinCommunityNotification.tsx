3963410ef3a6a8d7479ffeaa000b152ac0f670a1
Matevz <semprimoznik.matevz@gmail.com>
2023-06-13 16:46:04 +0200

    feat: add filter to notifications to show only unprocessed join requests

--- src/components/JoinCommunityNotification.tsx
+++ src/components/JoinCommunityNotification.tsx
@@ -1,46 +1,58 @@
-import React, {FC} from 'react';
-import {Text, View} from "react-native";
-import Button from "./Button";
+import React, { FC } from 'react';
+import { Text, View } from 'react-native';
+import Button from './Button';
 
-import Accordion from "./Accordion";
-import {JoinCommunityNotification as JoinCommunityNotificationType} from "../types/community.types";
-import useCommunityJoinRequestProcessMutation from "../hooks/useCommunityJoinRequestProcessMutation";
+import Accordion from './Accordion';
+import { JoinCommunityNotification as JoinCommunityNotificationType } from '../types/community.types';
+import useCommunityJoinRequestProcessMutation from '../hooks/useCommunityJoinRequestProcessMutation';
 
 interface NotificationCardProps {
-    notification: JoinCommunityNotificationType
+  notification: JoinCommunityNotificationType;
 }
-const JoinCommunityNotification: FC<NotificationCardProps> = ({notification}) => {
+const JoinCommunityNotification: FC<NotificationCardProps> = ({
+  notification,
+}) => {
+  const { mutate: declineJoinRequest, isLoading: isDeclineJoinRequestLoading } =
+    useCommunityJoinRequestProcessMutation();
+  const { mutate: acceptJoinRequest, isLoading: isAcceptJoinRequestLoading } =
+    useCommunityJoinRequestProcessMutation();
 
-    const {mutate: processJoinRequest} = useCommunityJoinRequestProcessMutation()
+  const onPressDeclineButton = () => {
+    declineJoinRequest({ accepted: false, notificationId: notification.id });
+  };
+  const onPressAcceptButton = () => {
+    acceptJoinRequest({ accepted: true, notificationId: notification.id });
+  };
 
-    const onPressProcessJoinRequest = (accepted: boolean) => {
-        processJoinRequest({accepted, notificationId: notification.id})
-    }
-
-    return (
-        <Accordion title='Prošnja za pridružitev' contentHeight={140} content={
-            <>
-                <Text className='text-white text-sm'>{notification.data.message}</Text>
-                <View className='flex flex-row justify-end w-full mt-5'>
-                    <Button
-                        text="Zavrni"
-                        classname='mr-3 w-22 h-11 bg-transparent border-2 border-white'
-                        classNameText='text-xs'
-                        onPress={() => onPressProcessJoinRequest(false)}
-                        loading={false}
-                    />
-                    <Button
-                        text="Sprejmi"
-                        classname='w-22 h-11'
-                        classNameText='text-xs'
-                        onPress={() => onPressProcessJoinRequest(true)}
-                        loading={false}
-                    />
-                </View>
-            </>
-        }/>
-
-    );
+  return (
+    <Accordion
+      title="Prošnja za pridružitev"
+      contentHeight={140}
+      content={
+        <>
+          <Text className="text-white text-sm">
+            {notification.data.message}
+          </Text>
+          <View className="flex flex-row justify-end w-full mt-5">
+            <Button
+              text="Zavrni"
+              classname="mr-3 w-22 h-11 bg-transparent border-2 border-white"
+              classNameText="text-xs"
+              onPress={onPressDeclineButton}
+              loading={isDeclineJoinRequestLoading}
+            />
+            <Button
+              text="Sprejmi"
+              classname="w-22 h-11"
+              classNameText="text-xs"
+              onPress={onPressAcceptButton}
+              loading={isAcceptJoinRequestLoading}
+            />
+          </View>
+        </>
+      }
+    />
+  );
 };
 
 export default JoinCommunityNotification;
