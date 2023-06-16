99845f148c9c27a3baec7f49a986e0f551383a1b
jernej10 <jernej.ofic@gmail.com>
2023-06-16 11:01:23 +0200

    fix: ui

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,43 +1,72 @@
-import React, {FC, ReactNode} from 'react';
-import {ActivityIndicator, Text, TouchableOpacity} from "react-native";
-import {twMerge} from "tailwind-merge";
-import Animated, {useAnimatedStyle, useSharedValue, withTiming} from "react-native-reanimated";
+import React, { FC, ReactNode } from 'react';
+import { ActivityIndicator, Text, TouchableOpacity } from 'react-native';
+import { twMerge } from 'tailwind-merge';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
 
 interface ButtonProps {
-    children?: ReactNode;
-    text: string;
-    onPress: () => void;
-    classname?: string;
-    disabled?: boolean;
-    classNameText?: string;
-    loading?: boolean
+  children?: ReactNode;
+  text: string | ReactNode;
+  onPress: () => void;
+  classname?: string;
+  disabled?: boolean;
+  classNameText?: string;
+  loading?: boolean;
 }
 
-const Button: FC<ButtonProps> = ({children, text, onPress, classname, classNameText, loading = false}) => {
-    const scale = useSharedValue(1)
+const Button: FC<ButtonProps> = ({
+  children,
+  text,
+  onPress,
+  classname,
+  classNameText,
+  loading = false,
+}) => {
+  const scale = useSharedValue(1);
 
-    const onPressIn = () => {
-        scale.value = withTiming(0.9, {duration: 100})
-    }
+  const onPressIn = () => {
+    scale.value = withTiming(0.9, { duration: 100 });
+  };
 
-    const onPressOut = () => {
-        scale.value = withTiming(1, {duration: 100})
-    }
+  const onPressOut = () => {
+    scale.value = withTiming(1, { duration: 100 });
+  };
 
-    const animatedStyle = useAnimatedStyle(() => {
-        return {
-            transform: [{scale: scale.value}]
-        }
-    })
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ scale: scale.value }],
+    };
+  });
 
-    return (
-        <Animated.View className={twMerge('bg-tint rounded-md self-start flex items-center justify-center', classname)} style={animatedStyle}>
-            {loading ? <ActivityIndicator size={28} className='py-1.5 px-6' color='white'/> : <TouchableOpacity onPressOut={onPressOut} onPressIn={onPressIn} activeOpacity={0.7} onPress={onPress} className='py-2.5 px-4'>
-                <Text className={twMerge('text-white font-bold', classNameText)}>{text}</Text>
-                {children}
-            </TouchableOpacity>}
-        </Animated.View>
-    );
+  return (
+    <Animated.View
+      className={twMerge(
+        'bg-tint rounded-md self-start flex items-center justify-center',
+        classname
+      )}
+      style={animatedStyle}
+    >
+      {loading ? (
+        <ActivityIndicator size={28} className="py-1.5 px-6" color="white" />
+      ) : (
+        <TouchableOpacity
+          onPressOut={onPressOut}
+          onPressIn={onPressIn}
+          activeOpacity={0.7}
+          onPress={onPress}
+          className="py-2.5 px-4"
+        >
+          <Text className={twMerge('text-white font-bold', classNameText)}>
+            {text}
+          </Text>
+          {children}
+        </TouchableOpacity>
+      )}
+    </Animated.View>
+  );
 };
 
 export default Button;
