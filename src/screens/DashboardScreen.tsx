ed81bffccb73f4cddd69be71cf2c03853fe5a016
Matevz <semprimoznik.matevz@gmail.com>
2023-06-23 18:46:21 +0200

    feat: install material bottom tabs

--- src/screens/DashboardScreen.tsx
+++ src/screens/DashboardScreen.tsx
@@ -7,18 +7,23 @@ import Tabs from "../components/Tabs";
 import {usePowerPlantStore} from "../store/power-plant-store";
 import {QueryBoundaries} from "../components/QueryBoundaries";
 import CalibrationForm from "../components/CalibrationForm";
+import {createMaterialTopTabNavigator} from "@react-navigation/material-top-tabs";
+import CustomTabBar from "../navigation/CustomTabBar";
+
+const Tab = createMaterialTopTabNavigator();
 
 const DashboardScreen:FC = () => {
     const {activeTab, setActiveTab} = useDashboardTabsStore(state => state)
     const selectedPowerPlant = usePowerPlantStore(state => state.selectedPowerPlant)
 
     return (
-        <View className='dark:bg-dark-main flex-1 pt-2'>
+        <View className='dark:bg-dark-main flex-1 pt-2' collapsable={false}>
             <QueryBoundaries isLoading={!selectedPowerPlant}>
-                <Tabs tabs={Object.values(PowerPlantsTab)} onClickTab={setActiveTab} activeTab={activeTab}/>
-                    {activeTab === PowerPlantsTab.DASHBOARD && <PowerPlantDashboardTab />}
-                    {activeTab === PowerPlantsTab.SETTINGS && <PowerPlantSettingsTab />}
-                    {activeTab === PowerPlantsTab.CALIBRATION && <CalibrationForm />}
+                <Tab.Navigator screenOptions={{animationEnabled: false}} tabBar={(props) => <CustomTabBar {...props}/>}>
+                    <Tab.Screen name="Home" options={{title: 'Home'}} component={PowerPlantDashboardTab} />
+                    <Tab.Screen name="Settings"  options={{title: 'Settings'}} component={PowerPlantSettingsTab} />
+                    <Tab.Screen name="Calibration" options={{title: 'Calibration'}} component={CalibrationForm} />
+                </Tab.Navigator>
             </QueryBoundaries>
         </View>
     );
